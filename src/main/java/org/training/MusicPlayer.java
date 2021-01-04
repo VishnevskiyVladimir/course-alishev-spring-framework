ed63c192f629cecedfb13c10ec86556460b19396
Vladimir Vishnevskiy <vishnevskiy.vladimir.mbox@gmail.com>
2021-01-04 14:00:17 +0300

    add Dependency Injection example through constructor and getter
    simple example

--- src/main/java/org/training/MusicPlayer.java
+++ src/main/java/org/training/MusicPlayer.java
@@ -1,14 +1,26 @@
 package org.training;
 
 public class MusicPlayer {
-    private Music music;
+    private Music song1;
+    private Music song2;
+
+
 
     // IoC
     public MusicPlayer(Music music) {
-        this.music = music;
+        this.song1 = music;
     }
 
+
+
+    public void setSong2(Music song2) {
+        this.song2 = song2;
+    }
+
+
+
     public void playMusic() {
-        System.out.println("Playing: " + music.getSong());
+        System.out.println("This bean injected through constructor and defined in xml configuration. Value: " + song1.getSong());
+        System.out.println("This bean injected through setter and defined in xml configuration. Value: " + song2.getSong());
     }
 }
