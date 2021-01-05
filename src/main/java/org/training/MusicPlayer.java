f130b6427716782e264b0b6b4b76a436623291bb
Vladimir Vishnevskiy <vishnevskiy.vladimir.mbox@gmail.com>
2021-01-05 13:29:56 +0300

    add example of using @Qualifier
    Remember that bean has the same name as class, but first letter is lowercase.

--- src/main/java/org/training/MusicPlayer.java
+++ src/main/java/org/training/MusicPlayer.java
@@ -1,6 +1,7 @@
 package org.training;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 
@@ -11,15 +12,12 @@ public class MusicPlayer {
     private Music song1;
 
 
-    public void setSong1(Music song1) {
-        System.out.println("DI through setter");
-        this.song1 = song1;
-    }
 
-//    public MusicPlayer(Music music) {
-//        System.out.println("DI through constructor");
-//        this.song1 = music;
-//    }
+
+    public MusicPlayer(@Qualifier("classicalMusic")  Music music) {
+        System.out.println("DI through constructor");
+        this.song1 = music;
+    }
 
 
 
